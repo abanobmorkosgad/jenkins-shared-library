0a024ad6bf2438afffa75119fccf1371e958f344
Abanob Morkos <bibo@server.bibo.com>
2024-04-04 17:02:20 +0200

    update vars

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,10 @@
-def call(){
+def call(String imageName){
     echo "Building docker image.."
     withCredentials([usernamePassword(credentialsId: 'DockerCred', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh "docker build -t abanobmorkos10/java-maven:1.2 ."
+        sh "docker build -t  $imageName ."
         sh "docker login -u $USER -p $PASS"
-        sh "docker push abanobmorkos10/java-maven:1.2"
+        sh "docker push $imageName"
     }
 }
+
+abanobmorkos10/java-maven:1.2
