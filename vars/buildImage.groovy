01234d5bbb0e541dfb5e32256a9387737a1e1677
Abanob Morkos <bibo@server.bibo.com>
2024-04-08 15:40:42 +0200

    using parameters

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "Building docker image.."
     withCredentials([usernamePassword(credentialsId: 'DockerCred', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh "docker build -t abanobmorkos10/java-maven:1.2 ."
+        sh "docker build -t $imageNmae ."
         sh "docker login -u $USER -p $PASS"
-        sh "docker push abanobmorkos10/java-maven:1.2"
+        sh "docker push $imageName"
     }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "Building app.."
+    echo "Building app from branch $BRANCH_NAME"
     sh "mvn package"
 }
