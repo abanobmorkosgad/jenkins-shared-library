5b1525ac0898e6e30902fbd1d9794a4b19dcc8b7
Abanob Morkos <bibo@server.bibo.com>
2024-04-08 15:44:03 +0200

    using parameters

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,7 @@
 def call(String imageName) {
     echo "Building docker image.."
     withCredentials([usernamePassword(credentialsId: 'DockerCred', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh "docker build -t $imageNmae ."
+        sh "docker build -t $imageName ."
         sh "docker login -u $USER -p $PASS"
         sh "docker push $imageName"
     }
